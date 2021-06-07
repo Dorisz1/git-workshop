ba5cb437883b4b0e445a22c07cd1a1264ce9c2ca
Solymosi Dori <dorisz.solymosi@gmail.com>
2021-06-07 15:34:51 +0200

    c3

--- Greet.java
+++ Greet.java
@@ -2,6 +2,6 @@ package com.greenfoxacademy;
 
 public class Greet {
     public void greet(String parameter) {
-        System.out.println("Greetings!");
+        System.out.println("Greetings");
     }
 }
