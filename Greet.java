c9fb84011d52aee7c51b38a6ff87aea93a0ee62b
sz.balazs94 <sz.balazs94@gmail.com>
2021-06-07 15:33:46 +0200

    d3

--- Greet.java
+++ Greet.java
@@ -1,7 +1,7 @@
 package com.greenfoxacademy;
 
 public class Greet {
-    public void greet(String parameter) {
-        System.out.println(parameter);
+    public void greet() {
+        System.out.println("Greetings!");
     }
 }
