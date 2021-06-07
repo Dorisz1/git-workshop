4f292ca7704a872217411c07e56a7c1ac32c0100
Solymosi Dori <dorisz.solymosi@gmail.com>
2021-06-07 15:49:27 +0200

    c5

--- cheer.java
+++ cheer.java
@@ -1,11 +1,11 @@
 public class Cheer {
 
-    public void sayTeamName(String name) {
-        System.out.println(name);
+    public void sayTeamName(String name, number) {
+        System.out.println(name, number);
     }
 
     public static void main(String[] args) {
         Cheer cheer = new Cheer();
-        Cheer.sayTeamName("Group 7");
+        Cheer.sayTeamName("Group", "7");
     }
 }
