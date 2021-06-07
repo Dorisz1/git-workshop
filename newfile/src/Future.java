0c586df9136a1e78089d9ab7b8eec556545feea4
Kiss Dániel <kissdanielzzz@gmail.com>
2021-06-07 16:06:38 +0200

    a6

--- newfile/src/Future.java
+++ newfile/src/Future.java
@@ -1,5 +1,6 @@
 public class Future {
-    public void future(String horoscope){
-        System.out.println(horoscope);
+    public void future(String horoscope) {
+
+        System.out.println("Your future looks like" + horoscope);
     }
 }
